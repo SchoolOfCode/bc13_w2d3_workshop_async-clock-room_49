778770a840394a0f1f14c045d1dfb9570910eaf0
camillabrizzi <camillabrizzi@icloud.com>
2022-10-05 12:29:33 +0100

    clock with seconds in real time

--- main.js
+++ main.js
@@ -2,16 +2,25 @@ const hand = document.querySelector(".hand");
 
 //🔍 HINT: THis is an example of how to rotate the hand!
 
+//Create Clock 
 //use setInterval and hand in function that rotates clock hand as well as 1000 millisecs;
 //create function that rotates clock hand and put hand.style.transform = `rotate(1deg)` within it;
 
-
+////Get JS Date object - SUCCESS!
+//Explore Date Object
+//use getSecond()
+// get seconds in real time so they align with clock :)
 
 let degrees = 0;
+let seconds;
 
 function rotateClockHand() {
-    degrees += 6;
+    seconds = new Date().getSeconds();
+    console.log(seconds);
+    degrees = seconds * 6;
     hand.style.transform = `rotate(${degrees}deg)`;
+
 }
 
 setInterval(rotateClockHand, 1000);
+
