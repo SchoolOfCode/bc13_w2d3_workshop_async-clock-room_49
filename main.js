97bc41467747ca91d55d12aeecda660246f51b8d
camillabrizzi <camillabrizzi@icloud.com>
2022-10-05 11:49:49 +0100

    first attempt

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 const hand = document.querySelector(".hand");
 
-hand.style.transform = `rotate(90deg)`; //🔍 HINT: THis is an example of how to rotate the hand!
+//🔍 HINT: THis is an example of how to rotate the hand!
+
+//use setInterval and hand in function that rotates clock hand as well as 1000 millisecs;
+//create function that rotates clock hand and put hand.style.transform = `rotate(1deg)` within it;
+
+
+
+
+function rotateClockHand() {
+    for (let count = 1; count <= 60; count++) {
+        hand.style.transform = `rotate(6deg)`;
+    }
+}
+
+setInterval(rotateClockHand, 1000);
